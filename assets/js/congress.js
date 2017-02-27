6054047ba006b1745cb9daeb1893b51afcc3725f
Erick Montañez <codeams@gmail.com>
2017-02-27 14:40:12 -0600

    Adding styles

---
+++ assets/js/congress.js
@@ -0,0 +1,13 @@
+(function($) {
+
+  console.info( 'Starting congress.js' );
+
+  $.getJSON( 'conf.json', function( conf ) {
+    congress( conf );
+  });
+
+  function congress( conf ) {
+    console.log( conf.pipeline );
+  }
+
+})( jQuery );
