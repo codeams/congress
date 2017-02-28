546748abb8384d5d791f6e8e9ac0e917ef7cfe3d
Erick Montañez <codeams@gmail.com>
2017-02-28 00:08:47 -0600

    Adding responsive behaviour to registrations' buttons

--- registration.php
+++ registration.php
@@ -47,10 +47,10 @@
       </div>
 
       <div class='action-buttons align-center row'>
-        <div class='button-container small-6 text-right columns'>
-          <input type='button' class='button secondary large' value='Regresar'>
-        </div><div class='button-container small-6 text-left columns'>
-          <input type='button' class='button primary large' value='Continuar'>
+        <div class='button-container small-12 medium-6 text-right columns'>
+          <input type='button' class='button secondary large' value='Cancelar'>
+        </div><div class='button-container small-12 medium-6 text-left columns'>
+          <input type='button' class='button primary large' value='Guardar'>
         </div>
       </div>
 
