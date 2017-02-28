346788834334b9db3fe1e79cb680a3147bbe3939
Erick Montañez <codeams@gmail.com>
2017-02-28 01:02:19 -0600

    Changing next button's text

--- registration.php
+++ registration.php
@@ -50,7 +50,7 @@
         <div class='button-container small-12 medium-6 text-right columns'>
           <input type='button' class='button secondary large' value='Cancelar'>
         </div><div class='button-container small-12 medium-6 text-left columns'>
-          <input type='button' class='button primary large' value='Guardar'>
+          <input type='button' class='button primary large' value='Continuar'>
         </div>
       </div>
 
