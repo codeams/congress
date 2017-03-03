bd727a050c5fcb0ee6203177b8990f657f5d4449
Erick Montañez <codeams@gmail.com>
2017-03-02 18:19:46 -0600

    Preparing unique template for the congress app

---
+++ app/components/section-navigation.js
@@ -0,0 +1,49 @@
+
+Vue.component( 'section-navigation', {
+
+
+  data: function() {
+    return {
+
+    }
+  },
+
+
+  template:
+
+  `
+    <div id='section-navigation'>
+
+      <div class='navigation align-center row'>
+
+        <div class='small-12 large-6 columns-collapsed'>
+
+          <span class='congress-title'>
+
+            Registro al <strong>CONISFOT</strong>
+
+          </span>
+
+        </div>
+
+        <div class='small-12 large-6 columns-collapsed'>
+
+          <span class='previous section instructions'></span>
+
+          <span class='previous section prices'></span>
+
+          <span class='current section registration'></span>
+
+          <span class='forward section validation'></span>
+
+          <span class='forward section voucher-submission'></span>
+
+        </div>
+
+      </div>
+
+    </div>
+  `
+
+
+})
