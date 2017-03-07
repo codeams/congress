eca405063cfcae26c3ef137f541c7f7616a6377c
Erick Montañez <codeams@gmail.com>
2017-03-06 18:07:31 -0600

    Applying 80 chars per line maximum, standard

--- app/components/instructions.js
+++ app/components/instructions.js
@@ -36,11 +36,40 @@ Vue.component( 'instructions', {
 
           <div class='small-12 columns'>
 
-            <p>1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <p>2. Morbi viverra posuere orci, sit amet pellentesque orci pellentesque at. Aenean massa quam, tristique et feugiat non, facilisis vitae sem. Proin vulputate feugiat velit, et convallis eros. Proin vulputate rhoncus ligula id tincidunt. Maecenas ornare venenatis mi finibus gravida. Fusce in nunc rhoncus, rutrum enim nec, bibendum sem.</p>
-            <p>3. Maecenas vehicula porttitor arcu a finibus. Mauris nibh tellus, facilisis nec justo eget, malesuada tempus quam. Donec vel hendrerit felis, at sodales dui. Suspendisse ut mauris at ipsum bibendum tempor at vel erat. Mauris gravida tincidunt lorem ac dapibus. Vestibulum sit amet nisl mauris.</p>
-            <p>4. Integer egestas, nisi bibendum eleifend blandit, metus mauris feugiat sem, id maximus dui leo sed libero. Donec at tellus ut orci imperdiet bibendum vel mattis leo. Suspendisse potenti. Vivamus viverra egestas arcu ut pellentesque. Donec molestie velit non ullamcorper iaculis. Cras interdum leo vitae sapien pretium laoreet.</p>
-            <p>5. Pellentesque ornare dolor vel massa euismod rhoncus. Suspendisse bibendum, magna non mollis porta, lorem purus sollicitudin nunc, vitae dictum velit dolor scelerisque lorem. Praesent posuere libero id nisl malesuada suscipit. Nam molestie turpis at luctus facilisis. Nullam luctus lacinia sem et viverra. Praesent eget vestibulum ex, at euismod nibh.</p>
+            <p>
+              1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <p>
+              2. Morbi viverra posuere orci, sit amet pellentesque orci
+              pellentesque at. Aenean massa quam, tristique et feugiat non,
+              facilisis vitae sem. Proin vulputate feugiat velit, et convallis
+              eros. Proin vulputate rhoncus ligula id tincidunt. Maecenas
+              ornare venenatis mi finibus gravida. Fusce in nunc rhoncus,
+              rutrum enim nec, bibendum sem.
+            </p>
+            <p>
+              3. Maecenas vehicula porttitor arcu a finibus. Mauris nibh tellus,
+               facilisis nec justo eget, malesuada tempus quam. Donec vel
+              hendrerit felis, at sodales dui. Suspendisse ut mauris at ipsum
+              bibendum tempor at vel erat. Mauris gravida tincidunt lorem ac
+              dapibus. Vestibulum sit amet nisl mauris.
+            </p>
+            <p>
+              4. Integer egestas, nisi bibendum eleifend blandit, metus mauris
+              feugiat sem, id maximus dui leo sed libero. Donec at tellus ut
+              orci imperdiet bibendum vel mattis leo. Suspendisse potenti.
+              Vivamus viverra egestas arcu ut pellentesque. Donec molestie velit
+               non ullamcorper iaculis. Cras interdum leo vitae sapien pretium
+               laoreet.
+            </p>
+            <p>
+              5. Pellentesque ornare dolor vel massa euismod rhoncus.
+              Suspendisse bibendum, magna non mollis porta, lorem purus
+              sollicitudin nunc, vitae dictum velit dolor scelerisque lorem.
+              Praesent posuere libero id nisl malesuada suscipit. Nam molestie
+              turpis at luctus facilisis. Nullam luctus lacinia sem et viverra.
+              Praesent eget vestibulum ex, at euismod nibh.
+            </p>
 
           </div>
 
