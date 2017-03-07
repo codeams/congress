d8c60ae9f104ed8bc5e7fd0867a513b9a761634e
Erick Montañez <codeams@gmail.com>
2017-03-07 15:53:52 -0600

    Removing terms of acceptance and data corroboration from both the model and the template

--- app/components/registration/terms-acceptance-form.js
+++ app/components/registration/terms-acceptance-form.js
@@ -6,9 +6,7 @@ Vue.component( 'terms-acceptance-form', {
 
     return {
 
-      dataCorrectness: false,
-      termsOfUse: false,
-      privacyPolicy: false
+      isPrivacyPolicyAccepted: false
 
     }
 
@@ -19,46 +17,17 @@ Vue.component( 'terms-acceptance-form', {
 
   `
     <div id='terms-acceptance-form'>
-
       <div class='row align-center'>
-
-        <!-- <div class='checkbox-container small-12 text-center columns'>
-
-          <label>
-
-            <input type = 'checkbox' v-model = ' dataCorrectness '>
-            Garantizo la veracidad de los datos proporcionados.
-
-          </label>
-
-        </div> -->
-
-
-        <!-- <div class='checkbox-container small-12 text-center columns'>
-
-          <label>
-
-            <input type = 'checkbox' v-model = ' termsOfUse '>
-            Estoy de acuerdo con los <a href='#'>términos de uso</a>.
-
-          </label>
-
-        </div> -->
-
-
         <div class='checkbox-container small-12 text-center columns'>
 
           <label>
-
-            <input type = 'checkbox' v-model = ' privacyPolicy '>
-            He leído y estoy de acuerdo con el <a href='#'>aviso de privacidad</a>.
-
+            <input type='checkbox' v-model='isPrivacyPolicyAccepted'>
+            He leído y estoy de acuerdo con el
+            <a href='#'>aviso de privacidad</a>.
           </label>
 
         </div>
-
       </div>
-
     </div>
   `
 
